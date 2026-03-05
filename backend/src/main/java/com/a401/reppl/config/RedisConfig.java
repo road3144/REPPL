810e3102bc6baf7c4d003967e57f5289ce5c8874
road3144 <fnfnfn258@gmail.com>
2026-03-05 10:45:16 +0900

    feat: Redis 설정 추가

---
+++ backend/src/main/java/com/a401/reppl/config/RedisConfig.java
@@ -0,0 +1,32 @@
+package com.a401.reppl.config;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.hash.Jackson2HashMapper;
+
+@Configuration
+public class RedisConfig {
+
+    @Bean
+    public StringRedisTemplate stringRedisTemplate(RedisConnectionFactory connectionFactory) {
+        return new StringRedisTemplate(connectionFactory);
+    }
+
+    @Bean
+    public ObjectMapper objectMapper() {
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.registerModule(new JavaTimeModule());
+        mapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+        return mapper;
+    }
+
+    @Bean
+    public Jackson2HashMapper hashMapper(ObjectMapper objectMapper) {
+        return new Jackson2HashMapper(objectMapper, true);  // flatten = true
+    }
+}
